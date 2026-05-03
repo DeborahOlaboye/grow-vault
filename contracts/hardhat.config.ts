4735786397e2af727d2effa3279c1ad7cf629863
DeborahOlaboye <deboraholaboye@gmail.com>
2026-05-03 15:00:00 +0100

    chore(contract): add Alfajores testnet to hardhat config

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -3,5 +3,8 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  networks: {
+    alfajores: { url: "https://alfajores-forno.celo-testnet.org", accounts: [], chainId: 44787 },
+  },
 };
 export default config;
