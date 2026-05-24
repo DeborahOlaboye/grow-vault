2dfdf39eeda3e21daccd4fafec7085c2bb5667c3
DeborahOlaboye <deboraholaboye@gmail.com>
2026-05-24 23:08:07 +0100

    feat(landing): scaffold LandingPage component shell

---
+++ frontend/components/LandingPage.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+export default function LandingPage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-violet-50 via-white to-violet-50 flex flex-col">
+    </div>
+  );
+}
